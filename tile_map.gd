e07e878364341feb248a320f8b4800e433a8f295
Andy <olshansa@gmail.com>
2025-01-14 15:33:02 -0500

    followed tutorial

--- tile_map.gd
+++ tile_map.gd
@@ -7,11 +7,30 @@ enum layers {
 }
 
 const green_block_atlas_pos = Vector2i(2,0)
+const boundary_block_atlas_pos = Vector2i(0,1)
 const blue_block_atlas_pos = Vector2i(0,0)
 const main_source = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	place_boundaries()
+	pass
+	
+func place_boundaries():
+	var offsets = [
+		Vector2i(0, -1),
+		Vector2i(0, 1),
+		Vector2i(-1, 0),
+		Vector2i(1, 0),
+	]
+	var used = get_used_cells(layers.level0)
+	for spot in used:
+		for offset in offsets:
+			var current_spot = spot + offset
+			if get_cell_source_id(layers.level0, current_spot) == -1:
+				set_cell(layers.level0, current_spot, main_source, boundary_block_atlas_pos)
+	
+func place_platform():
 	for y in range(3):
 		for x in range(3):
 			print(x, y)
